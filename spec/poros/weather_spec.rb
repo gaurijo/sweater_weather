8093ef4d77707ad79e65623bf1e5f6348c109245
Gauri Joshi <gauri728@gmail.com>
2022-09-24 15:59:09 -0700

    more tests

--- spec/poros/weather_spec.rb
+++ spec/poros/weather_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Weather do
   it "exists and has attributes" do 
     weather = WeatherFacade.get_weather(39.73, -104.98)
     expect(weather).to be_a(Weather)
+    expect(weather.id).to eq(nil)
+    expect(weather.type).to eq("forecast")
   end
 end
--- spec/requests/api/v1/weather_request_spec.rb
+++ spec/requests/api/v1/weather_request_spec.rb
@@ -6,5 +6,14 @@ RSpec.describe "Weather Request" do
     get "/api/v1/forecast?location=denver,co"
     
     expect(response).to be_successful
+
+    result = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(result).to be_a(Hash)
+    expect(result).to have_key(:attributes)
+    expect(result[:attributes]).to have_key(:id)
+    expect(result[:attributes]).to have_key(:type)
+    expect(result[:attributes]).to have_key(:current_weather)
+
+    current_weather = result[:attributes][:current_weather]
   end
 end
