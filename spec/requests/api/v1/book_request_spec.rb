c9e444719e5dc004cf8ac0e2a3af2f04d34e0d96
Gauri Joshi <gauri728@gmail.com>
2022-09-26 10:30:45 -0700

    test: book request api call has expected data types

--- spec/requests/api/v1/book_request_spec.rb
+++ spec/requests/api/v1/book_request_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper' 
 
 RSpec.describe "Book Request" do 
-  it "sends book data based on a location and quantity" do 
+  it "sends book data based on a location and quantity", :vcr do 
 
     get "/api/v1/book-search?location=denver,co&quantity=5"
-
     expect(response).to be_successful
+    
+    result = JSON.parse(response.body, symbolize_names: true)[:data]
+    # require 'pry'; binding.pry 
+
+    expect(result).to have_key(:id)
+    expect(result[:id]).to eq(nil)
+    expect(result).to have_key(:attributes)
+    expect(result[:attributes]).to be_a(Hash)
+    expect(result[:attributes]).to have_key(:destination)
+    expect(result[:attributes]).to have_key(:forecast)
+    expect(result[:attributes][:forecast]).to have_key(:summary)
+    expect(result[:attributes][:forecast]).to have_key(:temperature)
+    expect(result[:books]).to be_a(Array)
+    expect(result[:books].first).to be_a(Hash)
+    expect(result[:books].first).to have_key(:isbn)
+    expect(result[:books].first[:isbn]).to be_a(Array)
+    expect(result[:books].first).to have_key(:title)
+    expect(result[:books].first[:title]).to be_a(String)
+    expect(result[:books].first).to have_key(:publisher)
+    expect(result[:books].first[:publisher]).to be_a(Array)
+    expect(result[:books].length).to eq(5)
   end
 end
