ea3c5b6e65770cdd45dbee8b40446aab50397fa5
Gauri Joshi <gauri728@gmail.com>
2022-09-26 15:34:08 -0700

    fixture file for user json response

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,19 @@
 class Api::V1::UsersController < ApplicationController 
   def create 
-    if params[:password] == params[:password_confirmation]
-      user = User.create(params[:user_params])
-      render json: UserSerializer.new(user), status: 201
-    else
-      render status: 404
+      if params[:password] == params[:password_confirmation]
+        user = User.create(params[:user_params])
+        # render json: UserSerializer.new(user), status: 201
+        render json: UserSerializer.user_data(user)
+        # require 'pry'; binding.pry 
+      else
+        render status: 404
+      end
     end
+
+private 
+
+  def user_params 
+    params.permit(:email, :password, :password_confirmation, :auth_token)
   end
 
-  private 
-  def user_params 
-    params.permit(:email, :password, :password_confirmation)
-  end
 end
--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -3,19 +3,25 @@ require 'rails_helper'
 RSpec.describe "User Request" do 
   it "uses api call to create user data" do 
 
-        data = {
-                  email: "gauri@email.com",
-                  password: "test123",
-                  password_confirmation: "test123"
-                }
+  result = JSON.parse(File.read('spec/fixtures/user.json'), symbolize_names: true)
 
   headers = { "CONTENT_TYPE" => "application/json", "Accept" => "application/json"}
 
-  post "/api/v1/users", headers: headers, params: JSON.generate(data)
+  post "/api/v1/users", headers: headers, params: JSON.generate(result)
   
   expect(response).to be_successful
 
-  result = JSON.parse(response.body, symbolize_names: true)
+  # result = JSON.parse(response.body, symbolize_names: true)
   # require 'pry'; binding.pry 
+  expect(result[:data]).to be_a(Hash)
+  expect(result[:data]).to have_key(:type)
+  expect(result[:data]).to have_key(:id)
+  expect(result[:data]).to have_key(:attributes)
+  expect(result[:data][:id]).to be_a(String)
+  expect(result[:data][:attributes]).to be_a(Hash)
+  expect(result[:data][:attributes]).to have_key(:email)
+  expect(result[:data][:attributes]).to have_key(:api_key)
+  expect(result[:data][:attributes][:email]).to be_a(String)
+  expect(result[:data][:attributes][:api_key]).to be_a(String)
   end
 end
