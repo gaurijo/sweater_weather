754b74f15d37edda33187d816d67c1640b53522c
Gauri Joshi <gauri728@gmail.com>
2022-09-26 18:00:24 -0700

    DRY code - removes extra fixture file

--- spec/requests/api/v1/session_request_spec.rb
+++ spec/requests/api/v1/session_request_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Session Request" do 
-  it "uses api call to create a login session" do 
+  it "uses api call to create a login session for a user" do 
 
-    result = JSON.parse(File.read('spec/fixtures/session.json'), symbolize_names: true)
+    result = JSON.parse(File.read('spec/fixtures/user.json'), symbolize_names: true)
 
     header = { "CONTENT_TYPE" => "application/json", "Accept" => "application/json"}
 
@@ -11,6 +11,7 @@ RSpec.describe "Session Request" do
     # require 'pry'; binding.pry 
     expect(response).to be_successful
     expect(response.status).to eq(200)
+
     expect(result).to be_a(Hash)
     expect(result[:data]).to be_a(Hash)
     expect(result[:data][:type]).to eq("users")
@@ -19,6 +20,7 @@ RSpec.describe "Session Request" do
     expect(result[:data][:attributes]).to have_key(:email)
     expect(result[:data][:attributes]).to have_key(:api_key)
     expect(result[:data][:attributes]).to_not have_key(:password)
+    expect(result[:data][:attributes]).to_not have_key(:password_confirmation)
     expect(result[:data][:attributes][:email]).to be_a(String)
     expect(result[:data][:attributes][:api_key]).to be_a(String)
   end
