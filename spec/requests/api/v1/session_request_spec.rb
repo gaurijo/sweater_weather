f504b3909a7dd5936ad3a76f5f50322639589641
Gauri Joshi <gauri728@gmail.com>
2022-09-26 17:37:53 -0700

    test: session request returns expected data types and accounts for some sad path

--- spec/requests/api/v1/session_request_spec.rb
+++ spec/requests/api/v1/session_request_spec.rb
@@ -11,5 +11,15 @@ RSpec.describe "Session Request" do
     # require 'pry'; binding.pry 
     expect(response).to be_successful
     expect(response.status).to eq(200)
+    expect(result).to be_a(Hash)
+    expect(result[:data]).to be_a(Hash)
+    expect(result[:data][:type]).to eq("users")
+    expect(result[:data][:id]).to be_a(String)
+    expect(result[:data]).to have_key(:attributes)
+    expect(result[:data][:attributes]).to have_key(:email)
+    expect(result[:data][:attributes]).to have_key(:api_key)
+    expect(result[:data][:attributes]).to_not have_key(:password)
+    expect(result[:data][:attributes][:email]).to be_a(String)
+    expect(result[:data][:attributes][:api_key]).to be_a(String)
   end
 end
