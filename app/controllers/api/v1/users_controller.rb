ae439617d82404ec9f84fbc239b848e23e418870
Gauri Joshi <gauri728@gmail.com>
2022-09-28 15:31:45 -0700

    fixes user and sessions controller to get correct endpoints displaying in postman

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,17 @@
 class Api::V1::UsersController < ApplicationController 
   def create 
-    if params[:password] == params[:password_confirmation] 
-      user = User.create(params[:user_params])
+    user = User.new(user_params)
+    if user.save 
+      session[:user_id] = user.id 
       render json: UserSerializer.user_data(user), status: 201
     else 
       render json: { errors: "Email or password incorrect" }, status: 400
     end
   end
-
 private 
 
   def user_params 
     params.permit(:email, :password, :password_confirmation, :auth_token)
   end
 end
+
